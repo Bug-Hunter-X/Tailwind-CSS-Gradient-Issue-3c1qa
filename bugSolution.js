cad7cfa5aaed824951e2e61ef8d2feda8a167a38
BugHunter-X <askzenai@gmail.com>
2024-12-24 14:10:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="relative bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  <div class="absolute inset-0"></div>
+  <p>This is some text with a gradient background.</p>
+</div>
+```
